2a4c1238e18122654a5e4a533f4d5ae67b786b3c
Dan Carlstedt <dan.carlstedt@gmail.com>
2015-06-11 23:21:11 -0500

    adding done page and favicon

---
+++ ToDoApp/src/app/todo/done.controller.ts
@@ -0,0 +1,30 @@
+﻿module app.todo {
+    'use strict';
+
+    interface IDoneController {
+        doneItems: services.ITodoItem[];
+        totalDone: number;
+    }
+
+    class DoneController implements IDoneController {
+        doneItems: services.ITodoItem[];
+        totalDone: number;
+
+        static $inject: string[] = ['app.services.TodoService'];
+        constructor(private todoService: services.ITodoService) {
+            this.activate();
+        }
+
+        activate() {
+            // get all the done items
+            this.todoService.getDoneItems(  )
+                .then((items: services.ITodoItem[]) => {
+                this.doneItems = items;
+                this.totalDone = items.length;
+            });
+        }
+    }
+
+    angular.module('app.todo')
+        .controller('DoneController', DoneController);
+}
