191e39ed39943aa5d7a24674b55308f834113823
Dan Carlstedt <Dan.Carlstedt@fcsamerica.com>
2015-06-12 12:20:58 -0500

    removing two more .js files that were missed in last commit

--- ToDoApp/src/app/app.module.js
+++
@@ -1,6 +0,0 @@
-/// <reference path="../typings/tsd.d.ts" />
-(function () {
-    angular
-        .module('app', ['app.core', 'app.services', 'app.todo']);
-})();
-//# sourceMappingURL=app.module.js.map
